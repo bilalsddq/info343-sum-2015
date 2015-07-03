7d73fe8be21c103be8bff13a9f2a996a50fe8d75
bilalsddq <bilalsddq@hotmail.com>
2015-07-02 18:43:42 -0700

    Update common.js

--- tasks/js/common.js
+++ tasks/js/common.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //initialize Parse library with your application ID and your app's JavaScript library key
-Parse.initialize('your application id', 'your app JavaScript library key');
+Parse.initialize('n6UT5KWUqfDFkqj7WBLw44s5PndoOlP0Hf18v6L7', 'UHgMIQjzbtWeSBSNq1oYrOTC9HgINpco33S2lIfy');
 
 /**
  * Shows an error in an element on the page with the class 'error-message'
